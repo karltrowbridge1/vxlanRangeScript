729f2bd006fe3f992f92324ca0d33dd9964cbacc
karltrowbridge <kttrowbridge1s@semo.edu>
2024-03-04 14:22:34 -0600

    Created the bash script version

--- check_servers_prox.sh
+++ check_servers_prox.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# grab ip range
+
+# ping ip range for service
+# if there is a proxmox 
+
+server=$3
+for ((i=$1; i<=$2; i++)); do
+    target=$server+i
+    if nc -z $target; then
+        serverList+=$target+", "
+    fi
+done
+
+echo "$serverList"
