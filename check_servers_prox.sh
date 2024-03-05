3eb10a55ee6447a620a6a3c7577f839f8be6a39d
karltrowbridge <kttrowbridge1s@semo.edu>
2024-03-05 17:52:24 -0600

    started adding the progress bar

--- check_servers_prox.sh
+++ check_servers_prox.sh
@@ -8,6 +8,29 @@ for ((i=$1; i<=$2; i++)); do    # loop through the ip range given
     if nc -z -w 1 $target $4; then  # check if the tager is running a service on the specefied port
         serverList+=$target", "     # add to the list of servers
     fi
+
+
+    # Calculate the ratio
+    $divisor=3
+    ratio=$(echo "scale=2; $i / $divisor" | bc)
+
+    # Perform comparisons
+    if [ $(echo "$ratio < 1/3" | bc) -eq 1 ]; then
+        echo -ne '#####                     (33%)\r'
+    elif [ $(echo "$ratio >= 1/3 && $ratio < 2/3" | bc) -eq 1 ]; then
+        echo -ne '#############             (66%)\r'
+    else
+        echo -ne '#######################   (100%)\r'
+    fi
+
+    if [$i/$2<(1/3)]; then
+    echo -ne '#####                     (33%)\r'
+    elif [$i/$2<(2/3)]
+    echo -ne '#############             (66%)\r'
+    sleep 1
+    echo -ne '#######################   (100%)\r'
+    echo -ne '\n'
+
 done
 
 echo "$serverList"  # return the completed list
